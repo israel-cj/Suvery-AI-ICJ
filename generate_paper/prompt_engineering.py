41dd9f1dfd18f3fd56704c2164eb8f27a1e8e131
israel-cj <93375286+israel-cj@users.noreply.github.com>
2023-09-15 14:17:49 +0200

    Json file again

--- generate_paper/prompt_engineering.py
+++ generate_paper/prompt_engineering.py
@@ -15,7 +15,7 @@ The prompt format should be something like this:
 Write a survey paper as clear as posssible about the effectiveness of various scaffold cue presentation and methods to combine gradient approaches in neural tissue engineering for directed cell migration, with a focus on chemical, adhesive, mechanical, topographical, and electrical types of gradients.
 This survey paper must be generated in json format including the next structure:
 "
-[ # It is mandatory to be a json whithou any other variable name  
+paper = [ # It is mandatory the variable being called paper   
     {"heading": "Title", "text":"..."},
     {"heading": "Abstract", "text":"..."},
     {"heading": "Introduction", "text":"..."},
@@ -28,8 +28,7 @@ This survey paper must be generated in json format including the next structure:
 ]
 "
 
-Consider the max lenght of the paper 1500 words including references in the format above. The json format must be correct (every head should be close correctly). Always citing your sources, the format of the references should be the standar IEEE.
-Don't inlude the line breaks "\ n " among the sections of the paper.
+Consider the max lenght of the paper 1500 words including references, and the json variable must be called 'paper'. The json format must be correct (every head should be close correctly). Always citing your sources, the format of the references should be the standar IEEE
 ```end
 
 Each prompt generated ends with "```end" and starts with "```start".
@@ -106,15 +105,59 @@ def generate_iterations(generator_X, instructions, model, iterations):
         },
     ]
     i = 0
-    list_proposals = []
     while i < iterations:
 
         try:
             paper_json_string = generate_code(model, second_message, max_tokens=2000)
-            list_proposals.append(paper_json_string)
         except Exception as e:
             print("Error in LLM API." + str(e))
             time.sleep(60)  # Wait 1 minute before next request
             continue
+
+        try:
+            e = None
+            paper = run_llm_code(paper_json_string)
+        except Exception as e:
+            print(e)
+            paper = None
+
+        if e is not None:
+            second_message += [
+                {"role": "assistant", "content": f"You are an expert scientist assistant creating a paper of around 1500 words including references, not more. You answer only by creating a paper in the asked format. Answer as concisely as possible.\n {instructions}"},
+                {
+                    "role": "user",
+                    "content": f"""The json string object failed when converting the into a json, error type: {type(e)}, error: {str(e)}.\n 
+                            Generate again the paper in the correct json format indicated earlier:
+                                        ```start
+                                        """,
+                },
+            ]
+            continue
+        if e is None:
+            break
         i = i + 1
-    return paper_json_string
+
+    if paper is None:
+        message = [
+                {"role": "assistant", "content": f"You are an expert scientist assistant creating a paper of around 1700 words including references, not more. You answer only by creating a paper in the asked format. Answer as concisely as possible.\n {instructions}"},
+                {
+                    "role": "user",
+                    "content": f"""
+                     ```start
+                     {generator_X}
+                     ```end
+                                        """,
+                },
+        ]
+        try:
+            paper_json_string = generate_code(model, message, max_tokens=2000)
+        except Exception as e:
+            print("Error in LLM API." + str(e))
+            time.sleep(60)  # Wait 1 minute before next request
+        try:
+            paper = run_llm_code(paper_json_string)
+        except Exception as e:
+            print(e)
+            paper = None
+
+    return paper
